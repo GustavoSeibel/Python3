d258a62bae52f3de02fd3dc6aa155393d6cd2f78
GUSTAVO CANAREK SEIBEL <gustavo.canarek.seibel@ericsson.com>
2020-10-07 13:29:05 -0300

    While Counter 2 ways

---
+++ counter.py
@@ -0,0 +1,16 @@
+counter = 5
+while counter != 0:
+    print("Inside the loop.", counter)
+    counter -= 1
+print("Outside the loop.", counter)
+
+
+
+
+#another way to use counter
+
+counter = 5
+while counter:
+    print("Inside the loop.", counter)
+    counter -= 1
+print("Outside the loop.", counter)
